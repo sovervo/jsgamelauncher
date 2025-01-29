1bd95863798159eee318221ca048a00b7f40c8eb
Sheldon McGee <sheldon@tooshel.com>
2025-01-28 19:08:40 -0700

    added the config files for retrobat . . . new pr for a real installer script if there is interest

---
+++ systems/retrobat/run.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+REM Add something here that checks to see if node is installed
+
+REM cd "C:\RetroBat\emulators\jsgamelauncher\"
+cd /d "C:\RetroBat\emulators\jsgamelauncher\"
+
+node index.js %*
