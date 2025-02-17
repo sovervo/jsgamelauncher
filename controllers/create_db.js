a0878c9ae1edc1430676db09aff6157a9c00db38
Luis Montes <monteslu@gmail.com>
2025-02-17 11:47:04 -0700

    flip axes vertically/horizontally when specified

--- controllers/create_db.js
+++ controllers/create_db.js
@@ -7,10 +7,36 @@ import parseCfg from './parse_cfg.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const xmlFile = path.join(__dirname, './es_input.cfg')
+const xmlFile = path.join(__dirname, './knulli_es_input.cfg');
+const batoceraXmlFile = path.join(__dirname, './batocera_es_input.cfg');
+
+const uniqueMap = {};
+
+const delimiter = '____';
 
 async function createDb() {
   const cfgList = await parseCfg(xmlFile);
+  cfgList.forEach((cfg) => {
+    cfg.name = ('' + cfg.name).trim();
+    cfg.fromDB = 'knulli';
+    uniqueMap[cfg.name + delimiter + cfg.guid] = cfg;
+  });
+  console.log('knulli controller count', cfgList.length);
+
+  const batoceraCfgList = await parseCfg(batoceraXmlFile);
+  let batoceraCount = 0;
+  batoceraCfgList.forEach((cfg) => {
+    cfg.name = ('' + cfg.name).trim();
+    if (!uniqueMap[cfg.name + delimiter + cfg.guid]) {
+      cfg.fromDB = 'batocera';
+      uniqueMap[cfg.name + delimiter + cfg.guid] = cfg;
+      batoceraCount++;
+      cfgList.push(cfg);
+      console.log('batocera', cfg.name, cfg.guid);
+    }
+  });
+  console.log('batocera only controller count', batoceraCount);
+
   fs.writeFileSync(path.join(__dirname, './db.json'), JSON.stringify(cfgList));
   return cfgList;
 }
